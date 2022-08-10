82f363221c06604d1a84f5c6f37992cf947ebe9e
Daniel Margolin <dannyjmargolin@gmail.com>
2022-08-09 22:45:05 -0400

    express models done

--- models/express-models/todos.js
+++ models/express-models/todos.js
@@ -17,22 +17,32 @@ module.exports = {
 
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
-    // returns an array of all people for whom tasks exist
+    return Object.keys(tasks);
   },
 
   add: function (name, task) {
     // saves a task for a given person
+    if(tasks[name]) {
+      tasks[name].push(task);
+      if(!task.complete)task.complete = false;
+    } else {
+      tasks[name] = [task];
+      if(!task.complete)task.complete = false;
+    }
   },
 
   list: function (name) {
     // returns tasks for specified person
+    return tasks[name];
   },
 
   complete: function (name, idx) {
     // marks a task complete
+    tasks[name][idx].complete = true;
   },
 
   remove: function (name, idx) {
     // removes a tasks
+    tasks[name].splice(idx, 1);
   },
 };
