36cd669d1bb959f5bf0c66d8aacd4144766b820d
Daniel Margolin <dannyjmargolin@gmail.com>
2022-08-13 19:12:45 -0400

    updated extra credit

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,25 @@ const todos = require('../models/express-models/todos');
 module.exports = router;
 
 // write your routes here. Feel free to split into multiple files if you like.
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.send(todos.listPeople());
 });
 
-router.get('/:name/tasks', async(req, res, next) => {
-    if(req.query.status){
-        const tasks = todos.list(req.params.name)
-        let test = req.query.status === 'complete' ? tasks.filter(task => task.complete === true) : tasks.filter(task => task.complete === false);
-        res.send(test)
-    } else {
-    try {
-    const user = req.params.name;
-    const users = todos.listPeople();
-    if(await users.find(name => name === user)===undefined){
-        res.sendStatus(404);
-    } else {
-    res.send(todos.list(user));
-    }
-    } catch (err) {
-        next(err);
-    }
-}
+router.get('/:name/tasks', (req, res) => {
+        const user = req.params.name;
+        const users = todos.listPeople();
+        if(users.find(name => name === user)===undefined){
+            res.sendStatus(404);
+        } else if (req.query.status){
+            const tasks = todos.list(req.params.name)
+            let test = req.query.status === 'complete' ? tasks.filter(task => task.complete === true) : tasks.filter(task => task.complete === false);
+            res.send(test)
+        } else {
+            res.send(todos.list(user));
+        } 
 });
 
-router.post('/:name/tasks', (req, res, next) => {
+router.post('/:name/tasks', (req, res) => {
     if(req.body.content===''){
         res.sendStatus(400);
     } else {
@@ -37,12 +31,12 @@ router.post('/:name/tasks', (req, res, next) => {
     }
     });
 
-router.put('/:name/tasks/:index', (req, res, next) => {
-        todos.complete(req.params.name, req.params.index)
-        res.sendStatus(200);
+router.put('/:name/tasks/:index', (req, res) => {
+    todos.complete(req.params.name, req.params.index)
+    res.sendStatus(200);
 });
 
-router.delete('/:name/tasks/:index', (req, res, next) => {
+router.delete('/:name/tasks/:index', (req, res) => {
     todos.remove(req.params.name, req.params.index)
     res.sendStatus(204);
 });
